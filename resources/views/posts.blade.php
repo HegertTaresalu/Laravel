00af339c8635aee2912a85022c3dbd09b057e729
Opilane TTHK <opilane@tthk.ee>
2022-03-24 13:17:05 +0200

    add category filter

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,24 +1,14 @@
 <x-layout>
-    @foreach($posts as $post)
-    <article>
-        <h1>
-            <a href="/posts/{{ $post->slug}}">
-                {{ $post->title }}
-            </a>
-        </h1>
+    @include('_posts-header')
 
-        <p> By <a href="#">{{ $post->author->username }}</a>
-            in <a href="/categories/{{ $post->category->slug }}">
-                {{ $post->category->name }}
-            </a>
-        <div style="background-color: lightblue;margin-bottom:5px">
-            <a href="/categories/{{ $post->category->slug }}">
-                {{ $post->category->name }}
-            </a>
-        </div>
-        <div>
-            {!! $post->excerpt !!}
-        </div>
-    </article>
-    @endforeach
+    <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+        @if ($posts->count())
+
+        <x-posts-grid :posts="$posts"/>
+        @else
+            <p class="text-center">There are currently no posts. Please check back at a later date.</p>
+        @endif
+
+    </main>
+
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -18,12 +18,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
 Route::get('/', function () {
     return view('posts', [
-        "posts"=>Post::latest()->with("category","author")->get()
+        "posts"=>Post::latest()->with(["category","author"])->get(),
+        "categories"=>Category::all()
     ]);
 });
-
 Route::get('/posts/{post:slug}', function (Post $post) {
     return view('post', [
         "post"=> $post
@@ -31,15 +32,17 @@ Route::get('/posts/{post:slug}', function (Post $post) {
 });
 Route::get('/categories/{category:slug}', function (Category $category) {
     return view('posts', [
-        "posts"=> $category->posts->load(["category","author"])
+        "posts"=> $category->posts->load(["category","author"]),
+        "currentCategory" =>$category,
+        "categories"=>Category::all()
     ]);
 });
-
 Route::get('/authors/{author:username}', function (User $author) {
     return view('posts', [
         "posts"=> $author->posts->load(["category","author"])
     ]);
 });
+
 Route::get('/hello', function ()
 {
     return "Hello World";
