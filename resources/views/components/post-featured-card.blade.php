d73383983ca25fb4c55b5a4dd1fdff642fcd7d67
Opilane TTHK <opilane@tthk.ee>
2022-03-24 13:22:29 +0200

    add merge

--- resources/views/components/post-featured-card.blade.php
+++ resources/views/components/post-featured-card.blade.php
@@ -1,7 +1,7 @@
 @props(['post'])
 
 <article
-            class="transition-colors duration-300 hover:bg-gray-100 border border-black border-opacity-0 hover:border-opacity-5 rounded-xl">
+{{ $attributes->merge(['class'=> "transition-colors duration-300 hover:bg-gray-100 border border-black border-opacity-0 hover:border-opacity-5 rounded-xl"]) }}>
             <div class="py-6 px-5 lg:flex">
                 <div class="flex-1 lg:mr-8">
                     {{-- TODO --}}
